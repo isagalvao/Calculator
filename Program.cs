e4873f5152f15ce44f4b1225fa68ecfdead2d27d
isabelle galvão <isabellegalvao123@gmail.com>
2023-01-27 14:56:13 -0300

    adding subtraction

--- Program.cs
+++ Program.cs
@@ -4,19 +4,45 @@ namespace Calculator{
 
   class Program {
 
-    static void Main(string[] args)
-    { 
-        Console.Clear();
+    static void Main(string[] args){
+      Console.WriteLine("Bem Vindo ao Calculator!");
+      Subtracao();
+    }
+    static void Soma(){
+       Console.Clear();
 
-        Console.WriteLine("Digite o Primeiro Valor:");
+       Console.WriteLine("Digite o Primeiro Valor:");
         float v1 = float.Parse(Console.ReadLine());
-        
-        Console.WriteLine("Digite o Segundo Valor:");
+          
+       Console.WriteLine("Digite o Segundo Valor:");
         float v2 = float.Parse(Console.ReadLine());
-       float resultado = v1 + v2;
-        Console.WriteLine("");  
-        Console.WriteLine("O resultado da Soma é: " + resultado);
-        
+
+        float resultado = v1 + v2;
+
+      // Console vazio apenas para pular Linha
+      Console.WriteLine("");  
+
+      Console.WriteLine("O resultado da Soma é: " + resultado);
+      Console.ReadKey();
+    }
+
+    static void Subtracao(){
+      Console.Clear();
+
+      Console.WriteLine("Digite o Primeiro Valor:");
+      float v1 = float.Parse(Console.ReadLine());
+
+       Console.WriteLine("Digite o Segundo Valor:");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 - v2;
+
+      // Console vazio apenas para pular Linha
+      Console.WriteLine("");  
+
+      Console.WriteLine("O resultado da subtração é: " + resultado);
+      Console.ReadKey();
     }
   }
+  
 }
