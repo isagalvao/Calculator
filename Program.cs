94c063f82cde4ad4b87912d0673641186681243c
isabelle galvão <isabellegalvao123@gmail.com>
2023-01-27 15:19:38 -0300

    add division

--- Program.cs
+++ Program.cs
@@ -43,6 +43,23 @@ namespace Calculator{
       Console.WriteLine("O resultado da subtração é: " + resultado);
       Console.ReadKey();
     }
+     static void divisao(){
+      Console.Clear();
+
+      Console.WriteLine("Digite o Primeiro Valor:");
+      float v1 = float.Parse(Console.ReadLine());
+
+       Console.WriteLine("Digite o Segundo Valor:");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 / v2;
+
+      // Console vazio apenas para pular Linha
+      Console.WriteLine("");  
+
+      Console.WriteLine("O resultado da divisao é: " + resultado);
+      Console.ReadKey();
+     }
   }
   
 }
