20852e63dbc0deeb5faebbf20265211490d06720
isabelle galvão <isabellegalvao123@gmail.com>
2023-01-27 14:32:56 -0300

    sum

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator{
+
+  class Program {
+
+    static void Main(string[] args)
+    { 
+        Console.Clear();
+
+        Console.WriteLine("Digite o Primeiro Valor:");
+        float v1 = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digite o Segundo Valor:");
+        float v2 = float.Parse(Console.ReadLine());
+       float resultado = v1 + v2;
+        Console.WriteLine("");  
+        Console.WriteLine("O resultado da Soma é: " + resultado);
+        
+    }
+  }
+}
